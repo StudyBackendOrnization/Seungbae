80f827bb82b7edee86f2bd0f7d8ba8323989e921
seungbae <seungbae@stanworld.kr>
2022-04-02 16:43:51 +0900

    calculate init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("---------------------")
+	fmt.Println("        계산기       ")
+	fmt.Println("---------------------")
+
+	calculate()
+}
+
+func calculate() {
+
+	calculate()
+}
